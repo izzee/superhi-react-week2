77b43931b807774f30017cf0845f758b4707b09a
Isaac Chabon <ichabon@me.com>
2020-04-21 09:39:05 -0400

    set up basic input functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import './css/main.css';
-import Header from './Header'
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-    </div>
-  );
+
+const Header = () => (
+  <header className="header grid">
+    <h1>Jiffy</h1>
+  </header>
+)
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchTerm: ''
+    }
+  }
+
+  handleChange = event => {
+    const { value } = event.target;
+    this.setState((prevState,  ) => ({
+      ...prevState,
+      searchTerm: value
+    }));
+    if ( value.length > 2 ) {
+      console.log('this is a valid search term');
+    }
+  }
+
+  handleKeyPress = event => {
+    const { value } = event.target;
+    if (value.length > 2 && event.key === 'Enter') {
+      alert(`search for ${value}`)
+    }
+  }
+
+  render() {
+    const { searchTerm } = this.state;
+    return (
+      <div className="page">
+        <Header/>
+  
+        <div className="search grid">
+          {/* Stack of gif images */}
+          <input 
+            className="input grid-item" 
+            placeholder="Type something"
+            value={ searchTerm }
+            onChange={ this.handleChange }
+            onKeyPress={ this.handleKeyPress }/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Header = () => (
-  <header>
-    <h1>Hello and welcome to my site!</h1>
-  </header>
-)
-
-export default Header;
